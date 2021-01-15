bb24291841f27c3601ff5ff5cb3b128e5ace748a
Brent Meeusen <brentm@live.nl>
2021-01-15 17:59:54 +0100

    added comments

--- mastermind/app.js
+++ mastermind/app.js
@@ -1,24 +1,28 @@
+// Node requires
 var express = require("express");
 var http = require("http");
 var ws = require("ws");
 
+// Start server
 var port = process.argv[2] || 3000;
 var app = express();
 
+// My requires
 var Game = require("./game");
 
-
+// Websocket
 app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
+const server = http.createServer(app).listen(port);
+const wss = new websocket.Server({ server });
 
 
-
+// EXAMPLE CODE
 var Validation = require("./validation");
 var av = new Validation.valid();
 av.validate();
 
 
-
+// Redirects
 app.get("/", function(req, res) {
 	res.sendFile("public/splash_screen.html", { root: "./" });
 });
@@ -30,13 +34,6 @@ app.get("/*.js", function(req, res) {
 	res.send("Umm,, javascript???");
 	console.log(req, res);
 });
-
-
-app.get("/public/javascripts/client.js", function(req, res) {
-	res.send("Umm,, javascript??? 2");
-});
-
-
 app.get("/*", function(req, res) {
 	res.sendFile("public/splash_screen.html", { root: "./" });
 });
