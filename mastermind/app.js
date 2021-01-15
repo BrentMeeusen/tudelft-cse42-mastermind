75324d24bcd7565be3742cd393d183cd06705cab
Brent Meeusen <brentm@live.nl>
2021-01-15 16:58:30 +0100

    Added example for multiple server-side servers, added example redirects

--- mastermind/app.js
+++ mastermind/app.js
@@ -1,8 +1,31 @@
 var express = require("express");
 var http = require("http");
+var ws = require("ws");
 
-var port = process.argv[2];
+var port = process.argv[2] || 3000;
 var app = express();
+
 
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
+
+
+
+var Validation = require("./validation");
+var av = new Validation.valid();
+av.validate();
+
+
+
+app.get("/", function(req, res) {
+	res.sendFile("public/splash_screen.html", { root: "./" });
+});
+
+
+app.get("/something", function(req, res) {
+	res.send("/splash_screen.html");
+});
+
+
+
+
---
+++ mastermind/validation.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+
+class Validation {
+	
+	constructor() {
+
+	}
+
+	validate() {
+		console.log("Validating...");
+	}
+
+
+}
+
+// Validation.validate();
+
+exports.valid = Validation;
