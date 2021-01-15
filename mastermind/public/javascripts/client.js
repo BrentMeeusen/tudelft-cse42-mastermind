c13eec9d98d517ddf15863db3db522cab6269025
Brent Meeusen <brentm@live.nl>
2021-01-15 18:15:51 +0100

    improved websocket clientside

--- mastermind/public/javascripts/client.js
+++ mastermind/public/javascripts/client.js
@@ -1,13 +1,16 @@
 const socket = new WebSocket("ws://localhost:3000");
 
+// When the socket opens
+socket.onopen = function() {
+	socket.send("Client joined a game...");
+	console.info("Sending a message to the server...");
+}
+
+// When the socket receives a message
 socket.onmessage = function(event) {
 	console.info("Incoming message: ", event.data);
 	console.log("Raw object: ", event);
 }
 
-socket.onopen = function() {
-	socket.send("Client joined a game...");
-	console.info("Sending a message to the server...");
-}
 
 
