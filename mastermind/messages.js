131661bf71af247865b511ed131af18705cd42b4
Brent Meeusen <brentm@live.nl>
2021-01-18 16:05:38 +0100

    added new messages that are needed now

--- mastermind/messages.js
+++ mastermind/messages.js
@@ -1,6 +1,8 @@
 const messages = {
 	USER_CONNECTED: "User connected.",
 	USER_DISCONNECTED: "User disconnected.",
+	GAME_STARTS_MAKECODE: "The game has begun! Make your code...",
+	GAME_STARTS_GUESSCODE: "The game has begun! Wait for the other player to make a code...",
 }
 
 exports.messages = messages;
