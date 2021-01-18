277d6bce13e8dc8c85ae0b857d4c13ee015c3ae9
Brent Meeusen <brentm@live.nl>
2021-01-18 16:58:17 +0100

    Altered messages so that it works out on the client side

--- mastermind/messages.js
+++ mastermind/messages.js
@@ -1,10 +1,10 @@
 const messages = {
-	USER_CONNECTED: "User connected.",
-	USER_DISCONNECTED: "User disconnected.",
-	OPPONENT_DISCONNECTED: "Your opponent disconnected. The game has ended.",
-	WAITING_FOR_PLAYERS: "We're waiting for one other player to begin the game...",
-	GAME_STARTS_MAKECODE: "The game has begun! Make your code...",
-	GAME_STARTS_GUESSCODE: "The game has begun! Wait for the other player to make a code...",
+	USER_CONNECTED: { code: "USER_CONNECTED", message: "User connected" },
+	USER_DISCONNECTED: { code: "USER_DISCONNECTED", message: "User disconnected." },
+	OPPONENT_DISCONNECTED: { code: "OPPONENT_DISCONNECTED", message: "Your opponent disconnected. The game has ended." },
+	WAITING_FOR_PLAYERS: { code: "WAITING_FOR_PLAYERS", message: "We're waiting for one other player to begin the game..." },
+	GAME_STARTS_MAKECODE: { code: "GAME_STARTS_MAKECODE", message: "The game has begun! Make your code..." },
+	GAME_STARTS_GUESSCODE: { code: "GAME_STARTS_GUESSCODE", message: "The game has begun! Wait for the other player to make a code..." },
 }
 
 exports.messages = messages;
