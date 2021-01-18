04f116371c351bedd2018f1bceaf329350638082
Brent Meeusen <brentm@live.nl>
2021-01-18 21:36:56 +0100

    Added new message

--- mastermind/messages.js
+++ mastermind/messages.js
@@ -6,6 +6,7 @@ const messages = {
 	GAME_STARTS_MAKECODE: { code: "GAME_STARTS_MAKECODE", message: "The game has begun! Make your code..." },
 	GAME_STARTS_GUESSCODE: { code: "GAME_STARTS_GUESSCODE", message: "The game has begun! Wait for the other player to make a code..." },
 	OPPONENT_CREATED_CODE: { code: "OPPONENT_CREATED_CODE", message: "Your opponent created the code. Time to make a guess!" },
+	OPPONENT_MADE_GUESS: { code: "OPPONENT_MADE_GUESS", message: "Your opponent created the code. Time to make a guess!" },
 
 
 	ERRORS: {
