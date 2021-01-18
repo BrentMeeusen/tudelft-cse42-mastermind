558a153596979d4f6ff6329f7f733331788b79de
Brent Meeusen <brentm@live.nl>
2021-01-19 00:53:53 +0100

    added message for time synchronisation

--- mastermind/messages.js
+++ mastermind/messages.js
@@ -3,6 +3,7 @@ const messages = {
 	USER_DISCONNECTED: { code: "USER_DISCONNECTED", message: "User disconnected." },
 	OPPONENT_DISCONNECTED: { code: "OPPONENT_DISCONNECTED", message: "Your opponent disconnected. The game has ended." },
 	WAITING_FOR_PLAYERS: { code: "WAITING_FOR_PLAYERS", message: "We're waiting for one other player to begin the game..." },
+	GAME_STARTS: { code: "GAME_STARTS" },
 	GAME_STARTS_MAKECODE: { code: "GAME_STARTS_MAKECODE", message: "The game has begun! Make your code..." },
 	GAME_STARTS_GUESSCODE: { code: "GAME_STARTS_GUESSCODE", message: "The game has begun! Wait for the other player to make a code..." },
 	OPPONENT_CREATED_CODE: { code: "OPPONENT_CREATED_CODE", message: "Your opponent created the code. Time to make a guess!" },
