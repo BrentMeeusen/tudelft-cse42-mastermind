e0fb3bead211bb0e366dea9832dbb09efb385cab
florena_buse <i.buse@student.tudelft.nl>
2021-01-19 18:54:08 +0100

    FixedBothButtons

--- mastermind/public/javascripts/splash.js
+++ mastermind/public/javascripts/splash.js
@@ -6,35 +6,16 @@ document.getElementById("close-stats-popup").addEventListener("click", function(
 
 
 function toggleStats(forceOpen) {
-    console.log("Wheee")
     document.getElementById("stats").classList.toggle("active", forceOpen);
 }
 
 
+document.getElementById("close-howto-popup").addEventListener("click", function() {
+    toggleStats2(false);
+});
 
-/*
-const playButton = document.getElementById("play");
-playButton.onclick = function() {
-    alert('Finding available player...');
+
+function toggleStats2(forceOpen) {
+    document.getElementById("rules").classList.toggle("active", forceOpen);
 }
-*/
-
-/*
-let myVariable = document.querySelector('h1');
-myVariable.textContent = 'MASTERMIND';
-*/
-
-/*
-const div = document.querySelector('body');
-let winWidth = window.innerWidth;
-let winHeight = window.innerHeight;
-div.style.width = winWidth + 'px';
-div.style.height = winHeight + 'px';
-window.onresize = function() {
-    winWidth = window.innerWidth;
-    winHeight = window.innerHeight;
-    div.style.width = winWidth + 'px';
-    div.style.height = winHeight + 'px';
-  }
-  */
 
