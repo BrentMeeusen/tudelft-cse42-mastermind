d22a5008399ba94b001d44cb42978f7d956d7e1d
Brent Meeusen <brentm@live.nl>
2021-01-19 22:30:30 +0100

    added statistics class

---
+++ mastermind/stats.js
@@ -0,0 +1,14 @@
+class Stats {
+
+	constructor() {
+		this.playersOnline = 0;
+		this.playersInGame = 0;
+		this.gamesInProgress = 0;
+		this.totalGamesPlayed = 0;
+	}
+
+}
+
+
+
+exports.stats = Stats;
