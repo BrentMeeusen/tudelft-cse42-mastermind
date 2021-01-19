fd22e9cb3a3312e20b1b9c5890eeb056d4c3bdca
florena_buse <i.buse@student.tudelft.nl>
2021-01-19 11:43:55 +0100

    FindingPlayerScreen

--- mastermind/public/javascripts/splash.js
+++ mastermind/public/javascripts/splash.js
@@ -1,3 +1,4 @@
+//@ts-check
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
@@ -7,3 +8,10 @@ function myFunctionFirst() {
     var popup = document.getElementById("myPopupFirst");
     popup.classList.toggle("show");
 }
+
+/*
+const playButton = document.getElementById("play");
+playButton.onclick = function() {
+    alert('Finding available player...');
+}
+*/
