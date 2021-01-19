0d5f99ee4da9855dae762b3cc93754a3d06f0242
Brent Meeusen <brentm@live.nl>
2021-01-19 23:10:04 +0100

    Altered front-end to display statistics

--- mastermind/public/javascripts/splash.js
+++ mastermind/public/javascripts/splash.js
@@ -1,6 +1,18 @@
 //@ts-check
 const socket = new WebSocket("ws://localhost:3000");
 let messages = {};
+let statsInterval;
+
+
+// Request statistics every 15 seconds
+statsInterval = setInterval(function() {
+	
+	var m = { message: messages.REQUEST_STATS };
+	m = JSON.stringify(m);
+	socket.send(m);
+
+}, 15 * 1000);
+
 
 // ================================================================
 // When the socket receives a message
@@ -21,7 +33,30 @@ socket.onmessage = function(event) {
 
 	}
 
+	// If we get the stats
+	else if(MSG.message.code === "STATS") {
+
+		// Get the data...
+		var playersOnline = MSG.data.playersOnline;
+		var playersInGame = MSG.data.playersInGame;
+		var gamesInProgress = MSG.data.gamesInProgress;
+		var totalGamesPlayed = MSG.data.totalGamesPlayed;
+
+		// ...and display it
+		document.getElementById("players-online").innerHTML = playersOnline;
+		document.getElementById("players-ingame").innerHTML = playersInGame;
+		document.getElementById("games-in-progress").innerHTML = gamesInProgress;
+		document.getElementById("total-games-played").innerHTML = totalGamesPlayed;
+
+	}
+
 }
+
+
+
+
+
+
 
 
 
