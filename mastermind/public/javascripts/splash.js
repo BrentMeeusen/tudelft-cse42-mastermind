293bae2877327679b75c5cc5c5f1e084f21e593b
florena_buse <i.buse@student.tudelft.nl>
2021-01-19 17:50:46 +0100

    CloseButton

--- mastermind/public/javascripts/splash.js
+++ mastermind/public/javascripts/splash.js
@@ -1,13 +1,43 @@
 //@ts-check
 function myFunction() {
     var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+    popup.classList.add("show");
+    var closeButton = document.createElement('button');
+    closeButton.textContent = 'X';
+    closeButton.style.background = 'white';
+    closeButton.style.fontFamily = 'Bungee Shade';
+    closeButton.style.borderColor = '#AB23E0';
+    closeButton.style.borderWidth = "10px";
+    closeButton.style.borderRadius = '50%';
+    closeButton.style.position = 'absolute';
+    closeButton.style.right = '70px';
+    closeButton.style.top = '45px';
+    popup.appendChild(closeButton);
+    closeButton.onclick = function() {
+        popup.remove();
+    }
 }
 
 function myFunctionFirst() {
     var popup = document.getElementById("myPopupFirst");
-    popup.classList.toggle("show");
+    popup.classList.add("show");
+    var closeButton = document.createElement('button');
+    closeButton.textContent = 'X';
+    closeButton.style.background = 'white';
+    closeButton.style.fontFamily = 'Bungee Shade';
+    closeButton.style.borderColor = '#AB23E0';
+    closeButton.style.borderWidth = "10px";
+    closeButton.style.borderRadius = '50%';
+    closeButton.style.position = 'absolute';
+    closeButton.style.right = '70px';
+    closeButton.style.top = '45px';
+    popup.appendChild(closeButton);
+    var popup1 = popup;
+    closeButton.onclick = function() {
+        popup.remove();
+    }
 }
+
 
 /*
 const playButton = document.getElementById("play");
@@ -15,3 +45,23 @@ playButton.onclick = function() {
     alert('Finding available player...');
 }
 */
+
+/*
+let myVariable = document.querySelector('h1');
+myVariable.textContent = 'MASTERMIND';
+*/
+
+/*
+const div = document.querySelector('body');
+let winWidth = window.innerWidth;
+let winHeight = window.innerHeight;
+div.style.width = winWidth + 'px';
+div.style.height = winHeight + 'px';
+window.onresize = function() {
+    winWidth = window.innerWidth;
+    winHeight = window.innerHeight;
+    div.style.width = winWidth + 'px';
+    div.style.height = winHeight + 'px';
+  }
+  */
+
